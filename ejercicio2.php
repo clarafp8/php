49132e8e99619e0add3c52b0370e7841c9467350
clarafp8 <frigolet.paiva.clara@iescamas.es>
2025-09-22 10:36:47 +0200

    Realizado ejercicio 2

---
+++ ejercicio2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    $nombre="Clara";
+    $numero="648136158";
+    $direccion="Calle Sanlúcar la Mayor,1";
+    echo "Mi nombre es $nombre mi direccion es $direccion y mi numero de telefono es $numero ." 
+    ?>
+</body>
+</html>
