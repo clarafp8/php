f828acd7ee3c7c8f0c3a5458e0c894db91305336
clarafp8 <frigolet.paiva.clara@iescamas.es>
2025-09-22 10:52:39 +0200

    Hecho ejercicio3 tabla de palabras en inglés

---
+++ ejercicio3.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "  
+    <table border=1>
+        <th>
+        Español
+        </th>
+         <th>
+        Inglés
+        </th>
+            <tr>
+            <td>Casa</td>
+            <td>House</td>
+            </tr>
+            <tr>
+            <td>Mesa</td>
+            <td>Table</td>
+            </tr>
+            <tr>
+            <td>Silla</td>
+            <td>Chair</td>
+            </tr>
+            <tr>
+            <td>Portatil</td>
+            <td>Laptop</td>
+            </tr>
+            <tr>
+            <td>Sofá</td>
+            <td>Sofa</td>
+            </tr>
+            <tr>
+            <td>Árbol</td>
+            <td>Tree</td>
+            </tr>
+            <tr>
+            <td>Coche</td>
+            <td>Car</td>
+            </tr>
+            <tr>
+            <td>Libro</td>
+            <td>Book</td>
+            </tr>
+            <tr>
+            <td>Perro</td>
+            <td>Dog</td>
+            </tr>
+            <tr>
+            <td>Plátano</td>
+            <td>Banana</td>
+            </tr>
+               
+              
+    </table>
+    
+    ";
+   
+    ?>
+</body>
